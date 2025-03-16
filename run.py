158d8927c86e04bc4cc31a34ba486644d54b6d0f
Thibauld Braet <thibauld@live.be>
2025-03-16 20:52:58 +0100

    Comment logs

--- run.py
+++ run.py
@@ -1,24 +1,24 @@
 import os
 from app import create_app
 
-import os
-from app import create_app
+# import os
+# from app import create_app
 
-# Debugging - print all environment variables
-print("ENV VARIABLES TEST")
-for key, value in os.environ.items():
-    print(f"{key}: {value}")
+# # Debugging - print all environment variables
+# print("ENV VARIABLES TEST")
+# for key, value in os.environ.items():
+#     print(f"{key}: {value}")
 
-# Ensure FLASK_CONFIG is set
-config_name = os.getenv('FLASK_CONFIG')
-if not config_name:
-    raise ValueError("FLASK_CONFIG is not set!")
+# # Ensure FLASK_CONFIG is set
+# config_name = os.getenv('FLASK_CONFIG')
+# if not config_name:
+#     raise ValueError("FLASK_CONFIG is not set!")
 
-app = create_app(config_name)
+# app = create_app(config_name)
 
-if __name__ == '__main__':
-    port = int(os.environ["PORT"])  # Render requires this
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == '__main__':
+#     port = int(os.environ["PORT"])  # Render requires this
+#     app.run(host='0.0.0.0', port=port)
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
