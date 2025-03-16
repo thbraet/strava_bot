a54e559e0e9be8d6e450ab08f950a5cd37b40ebc
Thibauld Braet <thibauld@live.be>
2025-03-16 20:33:42 +0100

    Load dotenv

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT',5432))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, load_dotenv=True)
 
 
 
